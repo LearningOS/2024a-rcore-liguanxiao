2443b5d6ca3fe13e1b7d855340692bd4f0b494bc
liguanxiao <1137644127@qq.com>
2024-11-01 22:52:13 +0800

    lgx--commit

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -9,6 +9,10 @@ pub struct TaskControlBlock {
     pub task_status: TaskStatus,
     /// The task context
     pub task_cx: TaskContext,
+     /// The numbers of syscall called by task
+    pub syscall_times: [u32; MAX_SYSCALL_NUM],
+     /// first running time of task
+    pub first_time: usize,
 }
 
 /// The status of a task
